0b7e4f3c5392409dc6f79f362fdddc675b2c5bd7
jjgomera <jjgomera@gmail.com>
2014-05-06 00:04:00 +0200

    Add code and update readme

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(  
+    name = 'iapws',
+    version = '1.0.3',
+    py_modules = ['iapws'],
+    author = 'jjgomera',
+    author_email = 'jjgomera@gmail.com',
+    url = 'https://pypi.python.org/pypi/iapws',
+    description  = 'Python implementation of international-standard IAPWS-IF97 steam tables',
+    license = "gpl v3")
