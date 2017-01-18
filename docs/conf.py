4ae05c7e39e77dca2a4f16e1135f464bb3992c18
jjgomera <jjgomera@gmail.com>
2017-01-18 17:00:55 +0100

    Add scipyt to a doc dependendence

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,7 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
+print(os.path.abspath('./'))
 sys.path.insert(0, os.path.abspath('..'))
 
 # -- General configuration ------------------------------------------------
