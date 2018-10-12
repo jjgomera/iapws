64e41311d3f5537d013fcebeebc2b77d14d08f1c
jjgomera <jjgomera@gmail.com>
2018-10-12 11:25:17 +0200

    Change html documentation theme to reatthedocs to improve separationbetween function

--- docs/conf.py
+++ docs/conf.py
@@ -37,6 +37,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx.ext.autosummary',
+    'sphinx.ext.napoleon',
     'numpydoc'
 ]
 
@@ -118,7 +119,7 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'nature'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
