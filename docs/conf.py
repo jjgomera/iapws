83b517e5e5e674876b2e742c2f163122105e82e8
jjgomera <jjgomera@gmail.com>
2018-10-13 17:27:22 +0200

    Fix autoimport iapws in documentation

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,10 @@ import os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
 
+
+import iapws  # noqa
+
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
